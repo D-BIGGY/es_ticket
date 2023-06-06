30240fe35c6dd53429e89e733c6e3e2f1b8bd149
D-BIGGY <lorenzo.d.costa04@gmail.com>
2023-06-06 12:56:07 +0200

    idk

--- login.php
+++ login.php
@@ -92,7 +92,6 @@ session_start();
         signUpButton.addEventListener('click', () => {
             container.classList.add("right-panel-active");
         });
-
         signInButton.addEventListener('click', () => {
             container.classList.remove("right-panel-active");
         });
